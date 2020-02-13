dd1b200242bc1247948c4df707bb941f14b70616
Sebastian <56331266+SeabassJames@users.noreply.github.com>
2020-02-13 03:29:06 -0500

    Update main.js

--- HW0/js/main.js
+++ HW0/js/main.js
@@ -50,7 +50,7 @@ function create() {
     bg = game.add.tileSprite(0, 0, 1500, 600, 'background');
     //create goal
     goal = game.add.sprite(200, 0, 'goal');
-    goal.scale.setTo(0.5, 0.3);
+    goal.scale.setTo(0.5, 0.2);
     
     platforms = game.add.physicsGroup();
     
@@ -191,7 +191,7 @@ function render () {
 
      //game.debug.text(game.time.physicsElapsed, 32, 32);
      //game.debug.body(player);
-     //game.debug.bodyInfo(player, 16, 24);
+     game.debug.bodyInfo(player, 16, 24);
      //game.debug.body(leftWall);
 }
 
