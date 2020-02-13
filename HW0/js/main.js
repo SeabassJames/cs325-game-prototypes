6f755844b442572fe099d3b224de971e44faefd3
Sebastian <56331266+SeabassJames@users.noreply.github.com>
2020-02-13 01:58:31 -0500

    Update main.js
    turned on debug

--- HW0/js/main.js
+++ HW0/js/main.js
@@ -177,9 +177,9 @@ function render () {
     game.debug.text(game.time.suggestedFps, 32, 32);
 
     // game.debug.text(game.time.physicsElapsed, 32, 32);
-    // game.debug.body(player);
-    // game.debug.bodyInfo(player, 16, 24);
-
+     game.debug.body(player);
+     game.debug.bodyInfo(player, 16, 24);
+     game.debug.body(leftWall);
 }
 
     
