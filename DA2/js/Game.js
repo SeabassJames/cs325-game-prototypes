25993988e3c1c1e42421763c3cb8dfad45d76d12
Sebastian <56331266+SeabassJames@users.noreply.github.com>
2020-02-19 20:01:20 -0500

    Update Game.js
    Added semicolon

--- DA2/js/Game.js
+++ DA2/js/Game.js
@@ -33,7 +33,7 @@ BasicGame.Game = function (game) {
 };
 
 BasicGame.Game.prototype = {
-    var sprite
+    var sprite;
     create: function () {
         /*
         //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
