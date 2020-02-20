b5af368d9fb32e6733990b1e0ebacbb85042bda3
Sebastian <56331266+SeabassJames@users.noreply.github.com>
2020-02-19 20:39:30 -0500

    Update Game.js
    replaced game with this.game

--- DA2/js/Game.js
+++ DA2/js/Game.js
@@ -59,15 +59,15 @@ BasicGame.Game.prototype = {
         this.bouncy.inputEnabled = true;
         this.bouncy.events.onInputDown.add( function() { this.quitGame(); }, this );
         */
-        game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        game.stage.backgroundColor = '#0072bc';
+        this.game.stage.backgroundColor = '#0072bc';
 
-        this.bouncy = game.add.sprite(400, 300, 'logo');
+        this.bouncy = this.game.add.sprite(400, 300, 'logo');
         this.bouncy.anchor.setTo(0.5, 0.5);
 
         //  Enable Arcade Physics for the sprite
-        game.physics.enable(this.bouncy, Phaser.Physics.ARCADE);
+        this.game.physics.enable(this.bouncy, Phaser.Physics.ARCADE);
 
         //  Tell it we don't want physics to manage the rotation
         this.bouncy.body.allowRotation = false;
@@ -84,7 +84,7 @@ BasicGame.Game.prototype = {
         // This function returns the rotation angle that makes it visually match its
         // new trajectory.
         //this.bouncy.rotation = this.game.physics.arcade.accelerateToPointer( this.bouncy, this.game.input.activePointer, 5000, 5000, 5000 );
-        this.bouncy.rotation = game.physics.arcade.moveToPointer(this.bouncy, 60, this.game.input.activePointer, 500);
+        this.bouncy.rotation = this.game.physics.arcade.moveToPointer(this.bouncy, 60, this.game.input.activePointer, 500);
     },
 
     quitGame: function () {
