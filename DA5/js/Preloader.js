8f2acbc64324cd9ce730c90749a889e07b6dc1e3
Sebastian <56331266+SeabassJames@users.noreply.github.com>
2020-04-09 02:15:04 -0400

    Update Preloader.js
    Moved loading bar

--- DA5/js/Preloader.js
+++ DA5/js/Preloader.js
@@ -16,7 +16,7 @@ BasicGame.Preloader.prototype = {
 		//	These are the assets we loaded in Boot.js
 		//	A spooky ghost face background and a loading progress bar
 		this.background = this.add.sprite(0, 0, 'preloaderBackground');
-		this.preloadBar = this.add.sprite(300, 400, 'preloaderBar');
+		this.preloadBar = this.add.sprite(690, 450, 'preloaderBar');
 
 		//	This sets the preloadBar sprite as a loader sprite.
 		//	What that does is automatically crop the sprite from 0 to full-width
